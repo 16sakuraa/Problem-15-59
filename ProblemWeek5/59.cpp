f83d63296d6e33e984ce7a68df3434c8f1bc9f91
OAT <64010860@kmitl.ac.th>
2021-09-11 15:39:40 +0700

    Add project files.

---
+++ ProblemWeek5/59.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string.h>
+
+
+// 59 จงเขียนโปรแกรมรับตัวอักษร 1 ชุด แล้วแสดงผลลัพธ์จากหลังมาหน้า (Level 3)
+
+int main()
+{
+	char sen[100];
+	int i;
+	printf("Input : ");
+	scanf("%s",&sen);
+	printf("Output : ");
+	int length = strlen(sen);
+	
+	
+	
+	for (i = length; i >= 0; i--)
+	{
+		printf("%c", sen[i]);
+	}
+
+	
+	
+}
